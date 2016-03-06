2d0f4ca691b1e7cd8be61716659afcd5c690a378
Andrew Barta <andrew@idahobartas.com>
2016-03-06 14:57:59 -0700

    added Health bar and basic template of stuff

---
+++ holyBurger.js
@@ -0,0 +1,8 @@
+function changeBurgerHealth(newPercent)
+{
+    document.getElementById("burgerInnerHealth").style.width = newPercent + "%";
+}
+function changeEnemyHealth(newPercent)
+{
+    document.getElementById("enemyInnerHealth").style.width = newPercent + "%";
+}
