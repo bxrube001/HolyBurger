aaf6aa59f799b565f0b9e2328b016ecd3d7d5737
benjaminkimble <benjaminroykimble@gmail.com>
2016-03-06 16:52:28 -0900

    Added Comments
    Added comments to new scripts and html elements

--- holyBurger.js
+++ holyBurger.js
@@ -11,16 +11,22 @@ function changeEnemyHealth(newPercent)
     document.getElementById("enemyInnerHealth").style.width = newPercent + "%";
 }
 
+/****************************************************
+ * Sets up canvas and div by determining screen size
+ ****************************************************/
 function initializeScreen() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  document.getElementById("gameScreen").width = width * 0.98;
-  document.getElementById("gameScreen").height = height * 0.7;
-  document.getElementById("controlsDiv").style.width = width * 0.98;
-  document.getElementById("controlsDiv").style.height = height * 0.3;
+  width = window.innerWidth;                //get window width
+  height = window.innerHeight;              //get window height
+  document.getElementById("gameScreen").width = width * 0.98;  //set canvas width to 98%
+  document.getElementById("gameScreen").height = height * 0.7; //set canvas height to 70%
+  document.getElementById("controlsDiv").style.width = width * 0.98;  //set div width to 98% 
+  document.getElementById("controlsDiv").style.height = height * 0.3; //set div height to 30%
   loadHolyMolies(0);
 }
 
+/*******************************************************
+ * Updates Holy Molies. Takes a number.
+ *******************************************************/
 function loadHolyMolies(molies) {
   var screen = document.getElementById("gameScreen").getContext('2d');;
   screen.font = '24px "Slackey" cursive';
